f710d3bebf48a188b4d817c7ecfb44dd19444c10
Jaypee Luga <code.euler@gmail.com>
2024-06-07 02:49:47 +0800

    CommonCharacters Problem - Work In Progress
    Find Common Characters, and Duplicates on each word on an array.

---
+++ CommonCharacters/CommonCharacters/Program.cs
@@ -0,0 +1,28 @@
+﻿ IList<string> CommonChars(string[] words) {
+        
+        var chars = new Dictionary<string, int>();
+        foreach(var word in words)
+        {
+            var charArray = word.ToCharArray();
+            foreach(var c in charArray)
+            {
+                if(!chars.ContainsKey(c.ToString()))
+                {
+                    chars.Add(c.ToString(),1);
+                    continue;
+                }
+                
+                chars[c.ToString()]++;
+            }
+        }
+
+        var commonChars = chars.Where(x => x.Value == words.Length ).ToDictionary().Keys.ToList();
+        return commonChars;
+}
+
+Console.Clear();
+var result = CommonChars(new string[] { "bella", "label", "roller"});
+foreach(var word in result)
+{
+    Console.Write($"{word} ");
+}
