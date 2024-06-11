06de4077157619c9d3990dc64dae1482f0235c5a
Jaypee Luga <code.euler@gmail.com>
2024-06-11 21:40:06 +0800

    Leet Code Solution - Easy_TwoSum
    Solution for Easy - TwoSum exercise

---
+++ CodingExercises/CodingExercises/E_TwoNums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace E_TwoNums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new int[] { 2, 7, 11, 15 };
+        var target = 9;
+        var output = TwoSum(nums, target);
+        foreach(var i in output)
+            Console.WriteLine(i);
+    }
+
+    static int[] TwoSum(int[] nums, int target)
+    {
+        var indices = new List<int>();
+        for (var i=0; i<nums.Length; i++)
+        {
+           var x = target - nums[i];
+           var indexOfX = nums.ToList().IndexOf(x);
+
+           if (indexOfX != -1 && indexOfX != i)
+           {
+               indices.AddRange(new int[]{i,indexOfX});
+               break;
+           }
+        }
+
+        return indices.ToArray();
+    }
+}
