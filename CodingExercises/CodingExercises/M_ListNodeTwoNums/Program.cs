28a8c7c0060d97227fbe505c8b48c489fe961d42
Jaypee Luga <code.euler@gmail.com>
2024-06-22 22:08:28 +0800

    Synchup, Added solutions for RomanToInteger exercise, and Palindrome Integer Exercise

---
+++ CodingExercises/CodingExercises/M_ListNodeTwoNums/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace M_ListNodeTwoNums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // var tmp = new ListNode();
+        var head = new ListNode();
+        var tail = new ListNode();
+        var tmp = new ListNode();
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (head.next == null)
+                head = tmp;
+            
+            head.val = i;
+            head.next = tmp;
+        }
+        
+        while()
+    }
+}
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿        
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        
+        var x=-2147483648;
+        var isNegative = false;
+        if(x < 0)
+        {
+            isNegative = true;
+            x = x * -1;
+        }
+
+        var l = 0;
+        var r = x.ToString().Length - 1;
+        var xString = x.ToString().ToCharArray();
+        
+        while(l < r)
+        {
+            char tmp = ' ';
+            tmp = xString[l];
+            xString[l] = xString[r];
+            xString[r] = tmp;
+
+            l++;
+            r--;
+        }
+        Console.WriteLine(new string(xString));
+        var reversed = Int64.Parse(new string(xString));
+
+        if(isNegative)
+            reversed = reversed * -1;
+
+        if( x == reversed )
+            Console.WriteLine("Palindrome");
